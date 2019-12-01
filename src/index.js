c305c0c3d06aa5a9023e85441e7967154a3fb9a4
Sekwon Choi <schoi@netflix.com>
2019-11-30 18:16:57 -0800

    code push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  // babel will create a constructor from this
+  state = { lat: null, errorMessage: "" };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    console.log("componentDidMount");
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log("position acquired");
+        // we called setState!!!
+        this.setState({ lat: position.coords.latitude });
+
+        // we should never never nver do like
+        // this.state.lat = posotion.coords.latitude
+        // one single exception is the first initialization
+      },
+      err => {
+        console.log("error happened");
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage} </div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner text="checking location" />;
+  }
+
+  // React says we have to define render()
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
